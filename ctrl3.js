a278be58be96e29c7dd8730d70c04e4ad74818d4
Eric Quiao <quiao.ericfernand@gmail.com>
2022-02-20 08:35:33 +0800

    fixed excess and below 1 pages

--- ctrl3.js
+++ ctrl3.js
@@ -22,13 +22,87 @@ random_btn.addEventListener('click', getRandomComics);
 prev_btn.addEventListener('click', getPrevComics);
 next_btn.addEventListener('click', getNextComics);
 go_display.addEventListener('click', goDisplay);
-display.addEventListener('change',changeDisplay)
+display.addEventListener('change', changeDisplay);
 
-window.addEventListener("load", function () {
-  const loader = document.querySelector(".loader");
-  loader.className += " hidden"; // class "loader hidden"
+function goDisplay() {
+  let displayCount = Number(document.getElementById('display').value);
+  let pageNum = Number(document.getElementById('page_Num').value);
+  let lastDisplayPage = pageNum + (displayCount - 1) / 2;
+  let firstDisplayPage = pageNum - (displayCount - 1) / 2;
+
+  const requestUrl = `${corsHeader}/${apiUrl}/${apiUrlFormat}`;
+  fetch(requestUrl)
+    .then((res) => res.json())
+    .then((data) => {
+      let lastComic = data.num;
+      pageNumCheck(lastComic, pageNum, displayCount,lastDisplayPage,firstDisplayPage);
+    });
+}
+
+function pageNumCheck(lastComic, pageNum, displayCount,lastDisplayPage,firstDisplayPage) {
+  if (pageNum > lastComic) {
+    document.getElementById('page_Num').value = lastComic;
+    document.getElementById('display').value = Number(1);
+    alert(`latest comics is until ${lastComic}`);
+    displaySet(lastComic, 1);
+
+  } else if(pageNum < 1 ){
+    document.getElementById('page_Num').value = 1;
+    document.getElementById('display').value = Number(1);
+    alert(`first page is comic#1`);
+    displaySet(1, 1);
+  }else {
+    if(lastDisplayPage>lastComic){
+      setupCycle(lastDisplayPage,displayCount, lastComic)
+    }else if(firstDisplayPage<1){
+      if(displayCount==3){
+        document.getElementById('page_Num').value = 2;
+        document.getElementById('display').value = Number(3);
+        alert('comics with 3 display pages can only be set to minimum at comic#2')
+        displaySet(2,3)
+      }
+      if(displayCount==5){
+        document.getElementById('page_Num').value = 3;
+        document.getElementById('display').value = Number(5);
+        alert('comics with 5 display pages can only be set to minimum at comic#3')
+        displaySet(3,5)
+      }
+
+    }else{
+      displaySet(pageNum, displayCount)
+    }
+  }
+}
+
+function setupCycle(lastComic, displayCount, lastPage) {
+  let excess = lastComic - lastPage;
+  let covered = displayCount - excess;
+
+  let counter = 0;
+  let newCycle = 0;
+  let pagetoPost = lastPage - covered;
+
+  for (let i = 0; i < displayCount; i++) {
+    pagetoPost++;
+    counter++;
+    if (pagetoPost <= lastPage) {
+      displayComics2(pagetoPost, counter);
+    } else {
+      newCycle++;
+      displayComics2(newCycle, counter);
+    }
+  }
+}
+
+
+
+
+
+
+window.addEventListener('load', function () {
+  const loader = document.querySelector('.loader');
+  loader.className += ' hidden'; // class "loader hidden"
 });
-
 
 (function () {
   const requestUrl = `${corsHeader}/${apiUrl}/${apiUrlFormat}`;
@@ -40,9 +114,8 @@ window.addEventListener("load", function () {
       document.getElementById('page_Num').value = data.num;
       document.getElementById('display').value = Number(1);
 
-      let comicId = `?comicId=${data.num}`
-      urlUpdate(comicId)
-
+      let comicId = `?comicId=${data.num}`;
+      urlUpdate(comicId);
     });
 })();
 
@@ -51,106 +124,65 @@ function checkLastPage() {
   return fetch(requestUrl)
     .then((res) => res.json())
     .then((data) => {
-      return (data.num);
+      return data.num;
     });
 }
 
 function validateCurrentCycle() {
-
   checkLastPage().then((result) => {
-
-    let lastPage = result
+    let lastPage = result;
     let displayCount = Number(document.getElementById('display').value);
     let pageNum = Number(document.getElementById('page_Num').value);
 
-    let lastComic = pageNum + (displayCount-1)/2
+    let lastComic = pageNum + (displayCount - 1) / 2;
 
-    if(lastComic<=lastPage){
-      console.log('displayset')
-      displaySet(pageNum,displayCount)
-    }else{
-      console.log('setupCycle')
-      setupCycle(lastPage, pageNum, displayCount, lastComic)
-    }
+    if (lastComic <= lastPage) {
     
+      displaySet(pageNum, displayCount);
+    } else {
+     
+      setupCycle(lastPage, pageNum, displayCount, lastComic);
+    }
   });
 }
 
 function validateNextCycle() {
-
   checkLastPage().then((result) => {
-
-    let lastPage = result
+    let lastPage = result;
     let displayCount = Number(document.getElementById('display').value);
-    let pageNum = Number(document.getElementById('page_Num').value) + displayCount;
+    let pageNum =
+      Number(document.getElementById('page_Num').value) + displayCount;
 
-    let lastComic = pageNum + (displayCount-1)/2
+    let lastComic = pageNum + (displayCount - 1) / 2;
 
-     if(lastComic<=lastPage){
-      document.getElementById('page_Num').value = pageNum
-      displaySet(pageNum,displayCount)
-     }else{
-       setupNextCycle(lastPage, pageNum, displayCount, lastComic)
-     }
+    if (lastComic <= lastPage) {
+      document.getElementById('page_Num').value = pageNum;
+      displaySet(pageNum, displayCount);
+    } else {
+      setupNextCycle(lastPage, pageNum, displayCount, lastComic);
+    }
   });
 }
 
-function setupNextCycle(lastPage, pageNum, displayCount, lastComic){
+function setupNextCycle(lastPage, pageNum, displayCount, lastComic) {
 
-  console.log('here at setup')
 
-  console.log(pageNum)
-  console.log(lastPage)
-  console.log(displayCount)
-  console.log(lastComic)
-  
-    let updatedPageNum = pageNum - lastPage
-    console.log(updatedPageNum)
+  let updatedPageNum = pageNum - lastPage;
 
-    document.getElementById('page_Num').value = updatedPageNum
 
-    displaySet(updatedPageNum,displayCount)
-  }
+  document.getElementById('page_Num').value = updatedPageNum;
 
-function setupCycle(lastPage, pageNum, displayCount, lastComic){
-
-let excess = lastComic-lastPage;
-let covered = displayCount - excess;
-
-console.log(covered)
-console.log(excess)
-
-  let counter = 0;
-  let newCycle = 0;
-  let pagetoPost = lastPage - covered;
-  console.log(pagetoPost)
-
-  for (let i = 0; i < displayCount; i++) {
-    pagetoPost++;
-    counter++;
-    console.log(counter)
-    if (pagetoPost <= lastPage) {
-      displayComics2(pagetoPost, counter);
-     } 
-    else {
-       newCycle++;
-       displayComics2(newCycle, counter);
-     }
-  }
-}
-
-function goDisplay() {  
-  validateCurrentCycle()
-}
-
-function changeDisplay(){
-  goDisplay()
+  displaySet(updatedPageNum, displayCount);
 }
 
 
 
-function getNextComics(){
-  validateNextCycle()
+function changeDisplay() {
+  goDisplay();
+}
+
+function getNextComics() {
+  validateNextCycle();
 }
 
 function getPrevComics() {
@@ -159,8 +191,8 @@ function getPrevComics() {
 
   let nextMidPage = pageNum - displayCount;
 
-  if (nextMidPage <= 0){
-    nextMidPage = 1
+  if (nextMidPage <= 0) {
+    nextMidPage = 1;
   }
   document.getElementById('page_Num').value = nextMidPage;
 
@@ -169,7 +201,7 @@ function getPrevComics() {
 
 function getRandomComics() {
   let displayCount = Number(document.getElementById('display').value);
-  let randomNum = Math.ceil(Math.random() * 2583); //use ceiling and update 614
+  let randomNum = Math.ceil(Math.random() * 2581); //use ceiling and update 614
   let pageNum = randomNum;
 
   document.getElementById('page_Num').value = pageNum;
@@ -177,10 +209,9 @@ function getRandomComics() {
 }
 
 function setNumberOfDisplays() {
-  console.log('here at setNumberOfDisplays')
   let displayCount = Number(document.getElementById('display').value);
   let pageNum = Number(document.getElementById('page_Num').value);
-  
+
   displaySet(pageNum, displayCount);
 }
 
@@ -189,8 +220,8 @@ function displaySet(currentPage, displayCount) {
   let firstDisplay = currentPage - plusMinus;
   let lastDisplay = currentPage + plusMinus;
 
-  let comicId = `?comicId=${currentPage}`
-  urlUpdate(comicId)
+  let comicId = `?comicId=${currentPage}`;
+  urlUpdate(comicId);
   let counter = 0;
 
   for (let i = firstDisplay; i <= lastDisplay; i++) {
@@ -220,36 +251,28 @@ function displayComics2(comicNum, counter) {
   fetch(requestUrl)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data.img);
-      console.log(comicNum);
+
       if (numRef == 1) {
-
         first_display.innerHTML = `<img src = "${data.img}"/>`;
-        first_comic_num.innerHTML = comicNum        
-
+        first_comic_num.innerHTML = comicNum;
       }
       if (numRef == 2) {
-        
         second_display.innerHTML = `<img src = "${data.img}"/>`;
-        second_comic_num.innerHTML = comicNum   
+        second_comic_num.innerHTML = comicNum;
       }
       if (numRef == 3) {
-        
         third_display.innerHTML = `<img src = "${data.img}"/>`;
-        third_comic_num.innerHTML = comicNum   
+        third_comic_num.innerHTML = comicNum;
       }
       if (numRef == 4) {
-        
         fourth_display.innerHTML = `<img src = "${data.img}"/>`;
-        fourth_comic_num.innerHTML = comicNum   
+        fourth_comic_num.innerHTML = comicNum;
       }
       if (numRef == 5) {
-        
         fifth_display.innerHTML = `<img src = "${data.img}"/>`;
-        fifth_comic_num.innerHTML = comicNum   
+        fifth_comic_num.innerHTML = comicNum;
       }
       hideLoading();
-    
     });
 }
 
@@ -272,9 +295,6 @@ function hideLoading() {
   loader.classList.remove('display');
 }
 
-
-function urlUpdate(url){
-  console.log('hello')
-
-window.history.pushState('new','title', url)
+function urlUpdate(url) {
+  window.history.pushState('new', 'title', url);
 }
